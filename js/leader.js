215a5c51cb4af427161d15fb50244acd956446f7
Darío Natanael Carpio Arévalo <dncarpio@fiec.espol.edu.ec>
2017-11-23 22:43:00 -0500

    archivos avance 2

---
+++ js/leader.js
@@ -0,0 +1,9 @@
+/*
+$("h3").mouseover(function(){
+    $("h3").css("background-color", "yellow");
+});
+
+document.getElementsByClassName('nombre').addEventListener("mouseover", function(){
+	alert("Será redireccionado a Wikipedia");
+});
+*/
